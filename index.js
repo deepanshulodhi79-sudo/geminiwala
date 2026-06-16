0a54ff547c9a552f19da507f49c7d864bb8c1762
deepanshulodhi79-sudo <deepanshulodhi79@gmail.com>
2026-06-16 11:50:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -264,7 +264,8 @@ app.post('/api/send-bulk', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`UI Server running on port ${PORT}`);
+const PORT = process.env.PORT || 8080; 
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`UI Server running perfectly on port ${PORT}`);
 });
